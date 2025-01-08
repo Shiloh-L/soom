d8fb05ba675c4d3425c2feb5e9b0ddb7f2d74af3
Shiloh <1292949159@qq.com>
2025-01-08 21:56:38 +0800

    fix eslint

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,35 +1,35 @@
 'use client';
 
-import { useUser } from '@clerk/nextjs';
+import {useUser} from '@clerk/nextjs';
 import {
   StreamCall,
-  StreamTheme
+  StreamTheme,
 } from '@stream-io/video-react-sdk';
-import { useState } from 'react';
+import {useState} from 'react';
 import MeetingSetup from '@/components/MeetingSetup';
 import MeetingRoom from '@/components/MeetingRoom';
-import { useGetCallById } from '@/hooks/useGetCallById';
+import {useGetCallById} from '@/hooks/useGetCallById';
 import Loader from '@/components/Loader';
 
-const Meeting = ({ params }: { params: { id: string } }) => {
-  const { user, isLoaded } = useUser();
+const Meeting = ({params}: { params: { id: string } }) => {
+  const {isLoaded} = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallById(params.id);
+  const {call, isCallLoading} = useGetCallById(params.id);
 
   if (!isLoaded || isCallLoading) return <Loader />;
 
   return (
-    <main className='h-screen w-full'>
-      <StreamCall call={call}>
-        <StreamTheme>
-          {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
-          ) : (
-            <MeetingRoom />
-          )}
-        </StreamTheme>
-      </StreamCall>
-    </main>
+      <main className='h-screen w-full'>
+        <StreamCall call={call}>
+          <StreamTheme>
+            {!isSetupComplete ? (
+                <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+            ) : (
+                <MeetingRoom />
+            )}
+          </StreamTheme>
+        </StreamCall>
+      </main>
   );
 };
 
